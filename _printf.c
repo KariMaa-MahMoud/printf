e17af77aae31efd79e86e31c94dd0e2b5968a810
GergesYousef <111660433+GergesYousef@users.noreply.github.com>
2023-07-24 21:11:54 +0300

    Add files via upload

--- _printf.c
+++ _printf.c
@@ -1,13 +1,52 @@
-#include "main.h"
 #include <stdarg.h>
 #include <stdio.h>
-#include <stddef.h>
 #include <stdlib.h>
-
+#include "holberton.h"
+#include <stddef.h>
 /**
- * _printf - build a  printf function
- * @format: is a character string format
- * Returns: the number of characters printed (excluding the null byte)
+ * _printf - recreates the printf function
+ * @format: string with format specifier
+ * Return: number of characters printed
  */
+int _printf(const char *format, ...)
+{
+	if (format != NULL)
+	{
+		int count = 0, i;
+		int (*m)(va_list);
+		va_list args;
 
-
+		va_start(args, format);
+		i = 0;
+		if (format[0] == '%' && format[1] == '\0')
+			return (-1);
+		while (format != NULL && format[i] != '\0')
+		{
+			if (format[i] == '%')
+			{
+				if (format[i + 1] == '%')
+				{
+					count += _putchar(format[i]);
+					i += 2;
+				}
+				else
+				{
+					m = get_func(format[i + 1]);
+					if (m)
+						count += m(args);
+					else
+						count = _putchar(format[i]) + _putchar(format[i + 1]);
+					i += 2;
+				}
+			}
+			else
+			{
+				count += _putchar(format[i]);
+				i++;
+			}
+		}
+		va_end(args);
+		return (count);
+	}
+	return (-1);
+}
