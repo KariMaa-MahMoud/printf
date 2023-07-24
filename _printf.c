4ff9f42a504a1ae1abc75b22dda05d4110a2abc8
KariMaa-MahMoud <karima.mahmoud10@gmail.com>
2023-07-24 03:21:33 -0700

    Karima input to the first and second tasks in printf project

---
+++ _printf.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * _printf - build a  printf function
+ * @format: is a character string format
+ * Returns: the number of characters printed (excluding the null byte)
+ */
+
+
--- main.h
+++ main.h
@@ -14,11 +14,12 @@ typedef struct specifier
 	int (*f)(va_list);
 } spec;
 int _printf(const char *format, ...);
+int _putchar(char c);
+int (*get_func(char x))(va_list args);
 int print_c(va_list args);
 int print_s(va_list args);
+int print_percent(va_list args);
 int print_d(va_list args);
 int print_i(va_list args);
-int _putchar(char c);
-int print_percent(va_list args);
-int (*get_func(char x))(va_list args);
+
 #endif
